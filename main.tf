97d2a2d584e46b6cbad2fc5824c202fc5907ed83
Swatiz-cloud <swatizcloud@gmail.com>
2025-05-01 19:24:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
